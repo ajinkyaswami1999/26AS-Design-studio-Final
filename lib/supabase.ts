b17799785aecc29a4d11da4859deca5428029327
2ajinkya6 <158454550+ajinkyaswami1999@users.noreply.github.com>
2025-08-09 06:28:21 +0000

    supachnages0905

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,15 +3,13 @@ import { createClient } from '@supabase/supabase-js';
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 // const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabaseUrl = 'https://wqbqyipalgqzbzghjyyb.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables.');
 }
 
-export const supabase = supabaseUrl && supabaseKey 
-  ? createClient(supabaseUrl, supabaseKey)
-  : null;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Database types
 export interface Project {
