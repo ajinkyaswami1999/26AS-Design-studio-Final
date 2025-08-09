52ffba232b783d90f7c42c3f4149c4e534925049
2ajinkya6 <158454550+ajinkyaswami1999@users.noreply.github.com>
2025-08-09 06:03:58 +0000

    supachnages0903

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 // const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabaseUrl = 'https://wqbqyipalgqzbzghjyyb.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables.');
